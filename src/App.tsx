aa02169a58654bd5549865adb31b1802c7b6e7ca
Ricardo Gava <ricardoagava@gmail.com>
2022-10-16 12:04:28 -0300

    chore: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/global.css'
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1 className='font-bold text-4xl text-violet-500'>
+      Hello World!
+    </h1>
+  )
 }
-
-export default App;
